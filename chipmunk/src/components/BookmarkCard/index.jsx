0ca43e642122239aa4ae7c235c8150d6f70440cc
Jooyon Kim <gina611.kaist@gmail.com>
2020-06-04 00:37:10 +0900

    Link bookmark popup to bookmark card

--- chipmunk/src/components/BookmarkCard/index.jsx
+++ chipmunk/src/components/BookmarkCard/index.jsx
@@ -7,6 +7,7 @@ import CommentIcon from '@material-ui/icons/Comment';
 import IconButton from '@material-ui/core/IconButton';
 import StarRateIcon from '@material-ui/icons/StarRate';
 import BookmarkCardTag from "../BookmarkCardTag";
+import BookmarkPopup from "../BookmarkPopup";
 import "./BookmarkCard.scss";
 
 
@@ -71,6 +72,7 @@ export default function BookmarkCard() {
               <CommentIcon />
             </IconButton>
           </box>
+          <BookmarkPopup className={classes.info}></BookmarkPopup>
       </CardActions>
     </Card>
   );
