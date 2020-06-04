fb675c1060f9b3f765d9edc052315eff0279c93a
ChangWon Lee <changwon.lee@devsisters.com>
2020-06-05 01:39:57 +0900

    Add TopMenu using material-ui

---
+++ chipmunk/src/components/TopMenu/index.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import "./TopMenu.scss";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  toolbar: {
+    minHeight: 128,
+    background: "#7191A3",
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+  },
+}));
+
+export default function TopMenu() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h3" className={classes.title}>
+          My Cheek Pouch 
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
--- chipmunk/src/layout/index.jsx
+++ chipmunk/src/layout/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
 import "font-awesome/scss/font-awesome.scss";
-import Navigation from "../components/Navigation";
+import TopMenu from "../components/TopMenu";
 import config from "../../data/SiteConfig";
 import "./index.scss";
 import "./global.scss";
@@ -10,14 +10,14 @@ export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <Navigation config={config} LocalTitle={this.props.title}>
+      <TopMenu>
         <div>
           <Helmet>
             <meta name="description" content={config.siteDescription} />
           </Helmet>
           {children}
         </div>
-      </Navigation>
+      </TopMenu>
     );
   }
 }
