774b2d0a23bf7d4cdd4be1c9551503a2958e469f
ChangWon Lee <changwon.lee@devsisters.com>
2020-06-08 08:32:15 +0900

    Turn SearchBox into separate component

--- chipmunk/src/components/BookmarkMenuBar/index.jsx
+++ chipmunk/src/components/BookmarkMenuBar/index.jsx
@@ -1,158 +1,27 @@
 import React from "react";
-import { fade, useTheme, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import SearchIcon from '@material-ui/icons/Search';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputBase from '@material-ui/core/InputBase';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
-import CallMadeIcon from '@material-ui/icons/CallMade';
-import Typography from '@material-ui/core/Typography';
+import SearchBox from "../SearchBox";
 
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250,
-      },
-    },
-  };
-const names = [
-    'Oliver Hansen',
-    'Van Henry',
-    'April Tucker',
-    'Ralph Hubbard',
-    'Omar Alexander',
-    'Carlos Abbott',
-    'Miriam Wagner',
-    'Bradley Wilkerson',
-    'Virginia Andrews',
-    'Kelly Snyder',
-];
-
-function getStyles(name, personName, theme) {
-    return {
-      fontWeight:
-        personName.indexOf(name) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
-    };
-}
 
 const useStyles = makeStyles((theme) => ({
-    toolbar: {
-        marginLeft: 160,
-        backgroundColor: "#7191A3",
-        paddingTop: theme.spacing(1),
-        paddingBottom: theme.spacing(2),
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-          backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-          marginLeft: theme.spacing(1),
-          width: 'auto',
-        },
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-            width: '20ch',
-            },
-        },
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-        maxWidth: 300,
-    },
+  root: {
+    flexGrow: 1,
+  },
+  toolbar: {
+    marginLeft: 160,
+    backgroundColor: "#7191A3",
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+  },
 }));
 
 export default function TopMenu() {
     const classes = useStyles();
-    const theme = useTheme();
-    const [personName, setPersonName] = React.useState([]);
-
-    const handleChange = (event) => {
-        setPersonName(event.target.value);
-    };
     
     return (
         <Toolbar className={classes.toolbar}>
-            <Typography variant="h6" className={classes.title}>
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                    <SearchIcon />
-                    </div>
-                    <InputBase
-                    placeholder="Search…"
-                    classes={{
-                        root: classes.inputRoot,
-                        input: classes.inputInput,
-                    }}
-                    inputProps={{ 'aria-label': 'search' }}
-                    />
-                </div>
-                <div>
-                    <FormControl className={classes.formControl}>
-                        <InputLabel id="demo-mutiple-name-label">Name</InputLabel>
-                        <Select
-                        labelId="demo-mutiple-name-label"
-                        id="demo-mutiple-name"
-                        multiple
-                        value={personName}
-                        onChange={handleChange}
-                        input={<Input />}
-                        MenuProps={MenuProps}
-                        >
-                        {names.map((name) => (
-                            <MenuItem key={name} value={name} style={getStyles(name, personName, theme)}>
-                            {name}
-                            </MenuItem>
-                        ))}
-                        </Select>
-                    </FormControl>
-                </div>
-                <Button
-                    variant="contained"
-                    color="default"
-                    size="small"
-                    className={classes.button}
-                    startIcon={<CallMadeIcon />}
-                >
-                    Save
-                </Button>
-            </Typography>
+          <SearchBox></SearchBox>
         </Toolbar>
     );
 }
