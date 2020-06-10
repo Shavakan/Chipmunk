39ba75d35e86f287e404f955b7c8a105c4cf08c0
Park Junwoo <park.zoonoo@gmail.com>
2020-06-10 16:29:15 +0900

    Add license specification for vx example

--- chipmunk/src/components/BookmarkGraph/index.tsx
+++ chipmunk/src/components/BookmarkGraph/index.tsx
@@ -17,6 +17,22 @@ export type GraphProps = {
   setTree?: Function
 };
 
+// MIT License
+
+// Copyright (c) 2017-2018 Harrison Shoff
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// Code adapted from the example : 
+// https://github.com/hshoff/vx/blob/master/packages/vx-demo/src/sandboxes/vx-tree/Example.tsx
 const BookmarkGraph = function BookmarkGraph({ tree, width, height, margin }: GraphProps) {
   const data = useMemo(() => hierarchy(tree), [tree]);
   const yMax = height - margin.top - margin.bottom;
