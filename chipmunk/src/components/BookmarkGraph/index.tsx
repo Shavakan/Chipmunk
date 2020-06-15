7e875d52cab97f17b11b0c2e071e55019143dd7e
Park Junwoo <park.zoonoo@gmail.com>
2020-06-16 06:46:07 +0900

    fix for build

--- chipmunk/src/components/BookmarkGraph/index.tsx
+++ chipmunk/src/components/BookmarkGraph/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect, useReducer } from 'react';
+import React, { useMemo } from 'react';
 import { Group } from '@vx/group';
 import { Tree, hierarchy } from '@vx/hierarchy';
 import { LinkHorizontal } from '@vx/shape';
@@ -39,7 +39,6 @@ const BookmarkGraph = function BookmarkGraph({ tree, width, height, margin }: Gr
   return width < 10 ? null : (
     <svg id="bookmark-graph-svg" width={width+300} height={height} className="bookmark-graph">
       <LinearGradient id="lg" from={peach} to={pink} />
-      {/* <rect width={width} height={height} rx={14} fill={background} /> */}
       <Tree<TreeNode> root={data} size={[yMax, xMax]}>
         {tree => (
           <Group top={margin.top} left={margin.left}>
